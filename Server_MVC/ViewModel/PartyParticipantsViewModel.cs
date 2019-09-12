f63216e62c39be0741b01425a19e3394c79db624
Андрей М <librandron@gmail.com>
2019-09-12 21:12:29 +0300

    add structure

---
+++ Server_MVC/ViewModel/PartyParticipantsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server_MVC.ViewModel
+{
+    public class PartyParticipantsViewModel
+    {
+        public List<string> Participants { get; set; }
+        public string PartyName { get; set; }
+        public int PartyId { get; set; }
+    }
+}
